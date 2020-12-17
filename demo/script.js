e3a6723a8fef4e338e4102699fe097b7a9eb95e9
damiantoczek <damiantoczekprivat@gmail.com>
2020-12-17 08:41:54 +0100

    initial commit.

--- demo/script.js
+++ demo/script.js
@@ -1,9 +1,3 @@
-# tinySlider
-Simple image slider using VanillaJS, including full Demo.
-It preloads next image to be gentle for mobile devices.
-
-# Setup
-```javascript
 const slider = new Slider('div.slider', [
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_1.jpg",
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_2.jpg",
@@ -16,34 +10,21 @@ const slider = new Slider('div.slider', [
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_9.jpg",
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_10.jpg"
   ]);
-```
 
-Slider must be initiated to work, it was initially made to be used with window.readyState.
-```javascript
-slider.init();
-```
-
-# Recommended
-```javascript
 document.onreadystatechange = function(){
   let domState = document.readyState;
 
   if(domState === 'interactive'){
-    // After the HTML has been parsed and body elements are available.
     slider.init();
-
-    // Setting up the controll buttons, optional, the interval/auto sliding will still work.
     slider.root.querySelector('.controlls .next').addEventListener('click', () => {
-      slider.next()
+      slider.next();
     });
     slider.root.querySelector('.controlls .prev').addEventListener('click', () => {
-      slider.prev()
+      slider.prev();
     });
   }else if(domState === 'complete'){
     document.body.classList.remove('preload');
-    // After everything has been done, start the interval.
-    // This is my preference because it's more important for the content to be loaded first.
+
     slider.interval(2000);
   }
 }
-```
